f57d68aa1de7d97fc8a1c05ceb3757a092c3d6e4
fwAlphA <boburbek200101@gmail.com>
2025-06-22 01:40:58 +0500

    WeeklyTaskGeometryGuru

---
+++ WeeklyTaskGeometryGuru/GeometryGuru/GeometricFormulas/Parallelogram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryGuru.GeometricFormulas
+{
+    internal class Parallelogram
+    {
+        public void ParallelogramSumPer() 
+        {
+            Console.WriteLine("______Perimeter Sum_______");
+            Console.WriteLine("==========================");
+            Console.Write("Enter side A: ");
+            int numA = int.Parse(Console.ReadLine());
+            Console.Write("Enter side B: ");
+            int numB = int.Parse(Console.ReadLine());
+            Console.WriteLine($"2 * {numA} + 2 * {numB} = {2*numA+2*numB}");
+            Console.WriteLine("==========================");
+        }
+
+        public void ParallelogramSumArea() 
+        {
+            Console.WriteLine("_________Area Sum_________");
+            Console.WriteLine("==========================");
+            Console.Write("Enter side B: ");
+            int numB = int.Parse(Console.ReadLine());
+            Console.Write("Enter side H: ");
+            int numH = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{numB} * {numH} = {numB * numH}");
+            Console.WriteLine("==========================");
+        }
+    }
+}
---
+++ WeeklyTaskGeometryGuru/GeometryGuru/GeometricFormulas/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryGuru.GeometricFormulas
+{
+    internal class Rectangle
+    {
+        public void RectangleSumPer() 
+        {
+            Console.WriteLine("______Perimeter Sum_______");
+            Console.WriteLine("==========================");
+            Console.Write("Enter side A: ");
+            int numA = int.Parse(Console.ReadLine());
+            Console.Write("Enter side B: ");
+            int numB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Perimeter: 2*{numA} + 2*{numB} = {2 * numA + 2 * numB}");
+            Console.WriteLine("==========================");
+        }
+
+        public void RectangleSumArea() 
+        {
+            Console.WriteLine("_________Area Sum_________");
+            Console.WriteLine("==========================");
+            Console.Write("Enter side A: ");
+            int numA = int.Parse(Console.ReadLine());
+            Console.Write("Enter side B: ");
+            int numB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Area: {numA} * {numB} = {numA * numB}");
+            Console.WriteLine("==========================");
+        }
+    }
+}
